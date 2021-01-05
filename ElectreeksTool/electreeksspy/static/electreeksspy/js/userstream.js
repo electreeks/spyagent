d4ce2440c48cd905ed306dfc9c49645e8e7dd689
Hans Umlauft <hans.umlauft@electreeks.de>
2021-01-05 14:45:28 +0100

    Rücksetzen - mit Ajax Handler

--- ElectreeksTool/electreeksspy/static/electreeksspy/js/userstream.js
+++ ElectreeksTool/electreeksspy/static/electreeksspy/js/userstream.js
@@ -1,14 +1,28 @@
 window.addEventListener("load", function(){
 
   var video = document.querySelector("#userStream");
+  var jshandler = document.querySelector("#jshandler");
+  var stupid = "{{ stupid }}";
 
+  let refresh = setInterval(function() {
+    $('#jshandler2').load(document.URL +  ' #jshandler2');
+  }, 2000);
+ /*
+  let refresh = setInterval(function() {
+    $('#jshandler').load(document.URL +  ' #jshandler');
+  }, 2000);
+*/
   if (navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true })
       .then(function (stream) {
         video.srcObject = stream;
+
       })
       .catch(function (err0r) {
         alert("Something went wrong! Check if you allowed Electreeks to access your camera.");
       });
   }
+
+
+
 });
